f05dc3a577dc2954d6b1b142a9cf4da601b07c0b
Patrick H. | BCA CS <34520180+pathaj21@users.noreply.github.com>
2021-02-04 11:12:31 -0500

    Website Page File Creation
    Nothing on the pages yet

---
+++ statistics-website/src/components/pages/LeagueSchedules.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../App.css";
+
+export default function LeagueSchedules() {
+  return <h1 className="league-schedules">LEAGUE SCHEDULES</h1>;
+}
---
+++ statistics-website/src/components/pages/Videos.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+
+function Videos() {
+  return (
+    <>
+      <Cards />
+    </>
+  );
+}
+
+export default Videos;
