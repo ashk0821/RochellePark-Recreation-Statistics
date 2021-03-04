6ed6450dd0a5b58f839c5d40dac4c5f2a65649a8
AmaniIslam <amanii2408@gmail.com>
2021-03-04 10:55:20 -0500

    Navbar formatting fix

--- statistics-website/src/components/Navbar.js
+++ statistics-website/src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import { Button } from "./Button";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
@@ -18,14 +18,19 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton();
+  }, []);
+
   window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            STATS <i className="fab fa-typo3" />
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            STATS
+            <i class="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
@@ -60,24 +65,11 @@ function Navbar() {
                 className="nav-links"
                 onClick={closeMobileMenu}
               >
-                League Schedules
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/sign-up"
-                className="nav-links-mobile"
-                onClick={closeMobileMenu}
-              >
-                Sign Up
+                Schedules
               </Link>
             </li>
           </ul>
-          {button && (
-            <Button buttonStyle="btn--outline" path="/sign-up">
-              SIGN UP
-            </Button>
-          )}
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
