16658072e3fa676f62617ff003d1ff4f6cb53c3d
Patrick H. | BCA CS <34520180+pathaj21@users.noreply.github.com>
2021-05-07 09:36:30 -0400

    formatting and embedding of videos

--- statistics-website/src/components/Cards.js
+++ statistics-website/src/components/Cards.js
@@ -1,46 +1,113 @@
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import "./Cards.css";
-import CardItem from "./CardItem";
+//import CardItem from "./CardItem";
 import { Link } from "react-router-dom";
+
+function extract(src) {
+  var final = src.slice(-11);
+  return "https://www.youtube.com/embed/" + final;
+}
 
 function Cards() {
   return (
     <div className="cards">
       <h1>
-        {" "}
-        <Link to="/videos">Videos</Link>
+        <Link to="/videos" className="h-link">
+          Videos
+        </Link>
       </h1>
+
       <div className="cards__container">
-        <div className="cards__wrapper">
-          <ul className="cards__items">
-            <CardItem
-              src="https://i.ytimg.com/an_webp/uRAp00SxP30/mqdefault_6s.webp?du=3000&sqp=CLGS7oAG&rs=AOn4CLBCG8H1UW-ZNV-htZvZsZCiP-uvqw"
-              text="Derrick Rose Dunks - What are you doing Dragic?"
-              label="Dunk"
-              url="https://www.youtube.com/watch?v=uRAp00SxP30"
-            />
-            <CardItem
-              src="https://i.ytimg.com/an_webp/CqzGJMv-TmA/mqdefault_6s.webp?du=3000&sqp=CICm7oAG&rs=AOn4CLBfIz2alPYwbzFKooN2z2zeo__jTA"
-              text="Donovan Mitchell Sends Nerlens Noel to the Floor With NASTY Crossover"
-              label="Ankle Breaker"
-              url="https://www.youtube.com/watch?v=CqzGJMv-TmA"
-            />
-          </ul>
-          <ul className="cards__items">
-            <CardItem
-              src="https://i.ytimg.com/an_webp/5xvxWEx0sK8/mqdefault_6s.webp?du=3000&sqp=CJa87oAG&rs=AOn4CLDf0hZ0OgFIhTcb-uEG1T8WGfu9kQ"
-              text="Jeremy Lamb UNBELIEVABLE GAME-WINNER, SHOCKS THE WORLD | Hornets vs Raptors - March 24, 2019"
-              label="Game Winner"
-              url="https://www.youtube.com/watch?v=5xvxWEx0sK8"
-            />
-            <CardItem
-              src="https://i.ytimg.com/an_webp/e8KycyU47rs/mqdefault_6s.webp?du=3000&sqp=CMSZ7oAG&rs=AOn4CLCEedO-x5szkWeXCqMmtd-l3HnLlA"
-              text="Hakeem Olajuwon DESTROYS MVP Robinson! Full Game 6 Highlights vs Spurs 1995 WCF - EPIC!"
-              label="Highlights"
-              url="https://www.youtube.com/watch?v=e8KycyU47rs"
-            />
-          </ul>
-        </div>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className="vid"
+          width="385"
+          height="225"
+          src={extract("https://www.youtube.com/watch?v=uRAp00SxP30")}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
   );
